53ff10e3430f4cb2f8156502fb3b8d8bd5770d77
kangchihkuo <kangchihkuo@gmail.com>
2020-03-21 01:27:15 +0800

    Adding logic to delete items and sort items with productId.

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -1,8 +1,8 @@
-import { ADD_TO_CART } from "../actions/cart";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "../actions/cart";
 import CartItem from "../../models/cart-item";
 
 const initialState = {
-    items: [],
+    items: {},
     totalAmount: 0
 };
 
@@ -31,6 +31,30 @@ export default (state = initialState, action) => {
                 items: { ...state.items, [addedProduct.id]: updatedOrNewCartItem },
                 totalAmount: state.totalAmount + prodPrice
             };
+
+        case REMOVE_FROM_CART:
+            const selectedCartItem = state.items[action.pid];
+            const cureentQty = selectedCartItem.quantity;
+            let updatedCartItems;
+            if (cureentQty > 1) {
+                //need to reduce it, not erase it
+                const updatedCartItem = new CartItem(
+                    selectedCartItem.quantity - 1,
+                    selectedCartItem.productPrice,
+                    selectedCartItem.productTitle,
+                    selectedCartItem.sum - selectedCartItem.productPrice
+                );
+                updatedCartItems = { ...state.items, [action.pid]: updatedCartItem };
+            } else {
+                updatedCartItems = { ...state.items };
+                delete updatedCartItems[action.pid];
+            }
+            return {
+                ...state,
+                items: updatedCartItems,
+                totalAmount: state.totalAmount - selectedCartItem.productPrice
+            }
     }
+
     return state;
 };
