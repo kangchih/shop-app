e5ca6a0c511e4cbd369944d9d54b7d8b18f5acf6
kangchihkuo <kangchihkuo@gmail.com>
2020-03-23 02:05:08 +0800

    Handling user input.

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,20 +1,103 @@
-import React from 'react';
-import { View, Text, FlatList, Platform, Button } from 'react-native';
-// import { useSelector, useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, ScrollView, Platform } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 // import ProductItem from '../../components/shop/ProductItem';
-// import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-// import HeaderButton from '../../components/UI/HeaderButton';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../../components/UI/HeaderButton';
 // import Colors from '../../constants/Colors';
 // import * as productsActions from '../../store/actions/products';
 
 const EditProductsScreen = props => {
+    const prodId = props.navigation.getParam('productId');
+    const editedProduct = useSelector(state =>
+        state.products.userProducts.find(prod => prod.id === prodId)
+    );
+
+    const [title, setTitle] = useState(editedProduct ? editedProduct.title : '');
+    const [imageUrl, setImageUrl] = useState(editedProduct ? editedProduct.imageUrl : '');
+    const [price, setPrice] = useState('');
+    const [description, setDescription] = useState(editedProduct ? editedProduct.description : '');
     
     return (
-        <View>
-            <Text>The Edit Product Screen!</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.form}>
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Title</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={title}
+                        onChangeText={text => setTitle(text)}
+                    />
+                </View>
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Image Url</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={imageUrl}
+                        onChangeText={text => setImageUrl(text)}
+                    />
+                </View>
+                {editedProduct ? null : (
+                    <View style={styles.formControl}>
+                        <Text style={styles.label}>Price</Text>
+                        <TextInput
+                            style={styles.input}
+                            value={price}
+                            onChangeText={text => setPrice(text)}
+                        />
+                    </View>
+                )}
+                <View style={styles.formControl}>
+                    <Text style={styles.label}>Description</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={description}
+                        onChangeText={text => setDescription(text)}
+                    />
+                </View>
+            </View>
+        </ScrollView>
     )
-        
 };
+
+EditProductsScreen.navigationOptions = navData => {
+    return {
+        headerTitle: navData.navigation.getParam('productId')
+            ? 'Edit Product'
+            : 'Add Product',
+        headerRight: () => (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item
+                    title='Add'
+                    iconName={Platform.OS === 'android' ? 'md-checkmark' : 'ios-checkmark'}
+                    onPress={() => {
+                        // navData.navigation.navigate('EditProduct');
+                    }}
+                />
+            </HeaderButtons>
+        ),
+
+    };
+
+}
+
+const styles = StyleSheet.create({
+    form: {
+        margin: 20
+    },
+    formControl: {
+        width: '100%'
+    },
+    label: {
+        fontFamily: 'open-sans-bold',
+        marginVertical: 8
+    },
+    input: {
+        paddingHorizontal: 2,
+        paddingVertical: 5,
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1
+    }
+});
 
 export default EditProductsScreen;
